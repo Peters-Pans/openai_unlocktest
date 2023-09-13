3bc3beb503f792e8d9402ad09df0ea52877bd3ad
Peters-Pans <69051746+Peters-Pans@users.noreply.github.com>
2023-09-13 16:35:36 +0800

    Update Openai_Unlock.sh

--- Openai_Unlock.sh
+++ Openai_Unlock.sh
@@ -17,6 +17,93 @@ function Openai_UnlockTest() {
     echo "======================================="
 }
 
+function CheckV4() {
+    if [[ "$language" == "e" ]]; then
+        if [[ "$NetworkType" == "6" ]]; then
+            isv4=0
+            echo -e "${Font_SkyBlue}User Choose to Test Only IPv6 Results, Skipping IPv4 Testing...${Font_Suffix}"
+        else
+            echo -e " ${Font_SkyBlue}** Checking Results Under IPv4${Font_Suffix} "
+            check4=$(curl $curlArgs cloudflare.com/cdn-cgi/trace -4 -s 2>&1)
+            echo "--------------------------------"
+            echo -e " ${Font_SkyBlue}** Your Network Provider: ${local_isp4} (${local_ipv4_asterisk})${Font_Suffix} "
+            if [ -n  "$check4"  ]; then
+                isv4=1
+            else
+                echo -e "${Font_SkyBlue}No IPv4 Connectivity Found, Abort IPv4 Testing...${Font_Suffix}"
+                isv4=0
+            fi
+
+            echo ""
+        fi
+    else
+        if [[ "$NetworkType" == "6" ]]; then
+            isv4=0
+            echo -e "${Font_SkyBlue}用户选择只检测IPv6结果，跳过IPv4检测...${Font_Suffix}"
+        else
+            echo -e " ${Font_SkyBlue}** 正在测试IPv4解锁情况${Font_Suffix} "
+            check4=$(curl $curlArgs cloudflare.com/cdn-cgi/trace -4 -s 2>&1)
+            echo "--------------------------------"
+            echo -e " ${Font_SkyBlue}** 您的网络为: ${local_isp4} (${local_ipv4_asterisk})${Font_Suffix} "
+            if [ -n  "$check4"  ]; then
+                isv4=1
+            else
+                echo -e "${Font_SkyBlue}当前网络不支持IPv4,跳过...${Font_Suffix}"
+                isv4=0
+            fi
+
+            echo ""
+        fi
+    fi
+}
+
+function CheckV6() {
+    if [[ "$language" == "e" ]]; then
+        if [[ "$NetworkType" == "4" ]]; then
+            isv6=0
+            if [ -z "$usePROXY" ]; then
+                echo -e "${Font_SkyBlue}User Choose to Test Only IPv4 Results, Skipping IPv6 Testing...${Font_Suffix}"
+            fi
+        else
+            check6=$(curl $curlArgs cloudflare.com/cdn-cgi/trace -6 -s 2>&1)
+            if [ -n  "$check6"  ]; then
+                echo ""
+                echo ""
+                echo -e " ${Font_SkyBlue}** Checking Results Under IPv6${Font_Suffix} "
+                echo "--------------------------------"
+                echo -e " ${Font_SkyBlue}** Your Network Provider: ${local_isp6} (${local_ipv6_asterisk})${Font_Suffix} "
+                isv6=1
+            else
+                echo -e "${Font_SkyBlue}No IPv6 Connectivity Found, Abort IPv6 Testing...${Font_Suffix}"
+                isv6=0
+            fi
+            echo -e ""
+        fi
+
+    else
+        if [[ "$NetworkType" == "4" ]]; then
+            isv6=0
+            if [ -z "$usePROXY" ]; then
+                echo -e "${Font_SkyBlue}用户选择只检测IPv4结果，跳过IPv6检测...${Font_Suffix}"
+            fi
+        else
+            check6=$(curl $curlArgs cloudflare.com/cdn-cgi/trace -6 -s 2>&1)
+            if [ -n  "$check6"  ]; then
+                echo ""
+                echo ""
+                echo -e " ${Font_SkyBlue}** 正在测试IPv6解锁情况${Font_Suffix} "
+                echo "--------------------------------"
+                echo -e " ${Font_SkyBlue}** 您的网络为: ${local_isp6} (${local_ipv6_asterisk})${Font_Suffix} "
+                isv6=1
+            else
+                echo -e "${Font_SkyBlue}当前主机不支持IPv6,跳过...${Font_Suffix}"
+                isv6=0
+            fi
+            echo -e ""
+        fi
+    fi
+}
+
 
 function ScriptTitle() {
     if [[ "$language" == "e" ]]; then
