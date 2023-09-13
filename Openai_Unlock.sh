d2f639c57a1d9754128d8322740c1ef3f33691bc
Peters-Pans <69051746+Peters-Pans@users.noreply.github.com>
2023-09-13 16:45:08 +0800

    Update Openai_Unlock.sh

--- Openai_Unlock.sh
+++ Openai_Unlock.sh
@@ -1,3 +1,257 @@
+#!/bin/bash
+shopt -s expand_aliases
+Font_Black="\033[30m"
+Font_Red="\033[31m"
+Font_Green="\033[32m"
+Font_Yellow="\033[33m"
+Font_Blue="\033[34m"
+Font_Purple="\033[35m"
+Font_SkyBlue="\033[36m"
+Font_White="\033[37m"
+Font_Suffix="\033[0m"
+
+while getopts ":I:M:E:X:P:F:S:" optname; do
+    case "$optname" in
+        "I")
+            iface="$OPTARG"
+            useNIC="--interface $iface"
+        ;;
+        "M")
+            if [[ "$OPTARG" == "4" ]]; then
+                NetworkType=4
+                elif [[ "$OPTARG" == "6" ]]; then
+                NetworkType=6
+            fi
+        ;;
+        "E")
+            language="e"
+        ;;
+        "X")
+            XIP="$OPTARG"
+            xForward="--header X-Forwarded-For:$XIP"
+        ;;
+        "P")
+            proxy="$OPTARG"
+            usePROXY="-x $proxy"
+        ;;
+        "F")
+            func="$OPTARG"
+        ;;
+        "S")
+            Stype="$OPTARG"
+        ;;
+        ":")
+            echo "Unknown error while processing options"
+            exit 1
+        ;;
+    esac
+    
+done
+
+if [ -z "$iface" ]; then
+    useNIC=""
+fi
+
+if [ -z "$XIP" ]; then
+    xForward=""
+fi
+
+if [ -z "$proxy" ]; then
+    usePROXY=""
+fi
+
+if ! mktemp -u --suffix=RRC &>/dev/null; then
+    is_busybox=1
+fi
+curlArgs="$useNIC $usePROXY $xForward"
+UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64"
+UA_Dalvik="Dalvik/2.1.0 (Linux; U; Android 9; ALP-AL00 Build/HUAWEIALP-AL00)"
+Media_Cookie=$(curl -s --retry 3 --max-time 10 "https://raw.githubusercontent.com/1-stream/RegionRestrictionCheck/main/cookies")
+IATACode=$(curl -s --retry 3 --max-time 10 "https://raw.githubusercontent.com/1-stream/RegionRestrictionCheck/main/reference/IATACode.txt")
+
+countRunTimes() {
+    if [ "$is_busybox" == 1 ]; then
+        count_file=$(mktemp)
+    else
+        count_file=$(mktemp --suffix=RRC)
+    fi
+    RunTimes=$(curl -s --max-time 10 "https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2F1-stream%2FRegionRestrictionCheck&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" >"${count_file}")
+    TodayRunTimes=$(cat "${count_file}" | tail -3 | head -n 1 | awk '{print $5}')
+    TotalRunTimes=$(($(cat "${count_file}" | tail -3 | head -n 1 | awk '{print $7}') + 0))
+}
+countRunTimes
+
+checkOS() {
+    ifTermux=$(echo $PWD | grep termux)
+    ifMacOS=$(uname -a | grep Darwin)
+    if [ -n "$ifTermux" ]; then
+        os_version=Termux
+        is_termux=1
+        elif [ -n "$ifMacOS" ]; then
+        os_version=MacOS
+        is_macos=1
+    else
+        os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+    fi
+    
+    if [[ "$os_version" == "2004" ]] || [[ "$os_version" == "10" ]] || [[ "$os_version" == "11" ]]; then
+        is_windows=1
+        ssll="-k --ciphers DEFAULT@SECLEVEL=1"
+    fi
+    
+    if [ "$(which apt 2>/dev/null)" ]; then
+        InstallMethod="apt"
+        is_debian=1
+        elif [ "$(which dnf 2>/dev/null)" ] || [ "$(which yum 2>/dev/null)" ]; then
+        InstallMethod="yum"
+        is_redhat=1
+        elif [[ "$os_version" == "Termux" ]]; then
+        InstallMethod="pkg"
+        elif [[ "$os_version" == "MacOS" ]]; then
+        InstallMethod="brew"
+    fi
+}
+checkOS
+
+checkCPU() {
+    CPUArch=$(uname -m)
+    if [[ "$CPUArch" == "aarch64" ]]; then
+        arch=_arm64
+        elif [[ "$CPUArch" == "i686" ]]; then
+        arch=_i686
+        elif [[ "$CPUArch" == "arm" ]]; then
+        arch=_arm
+        elif [[ "$CPUArch" == "x86_64" ]] && [ -n "$ifMacOS" ]; then
+        arch=_darwin
+    fi
+}
+checkCPU
+
+checkDependencies() {
+    
+    # os_detail=$(cat /etc/os-release 2> /dev/null)
+    
+    if ! command -v python &>/dev/null; then
+        if command -v python3 &>/dev/null; then
+            alias python="python3"
+        else
+            if [ "$is_debian" == 1 ]; then
+                echo -e "${Font_Green}Installing python3${Font_Suffix}"
+                $InstallMethod update >/dev/null 2>&1
+                $InstallMethod install python3 -y >/dev/null 2>&1
+                alias python="python3"
+                elif [ "$is_redhat" == 1 ]; then
+                echo -e "${Font_Green}Installing python3${Font_Suffix}"
+                if [[ "$os_version" -gt 7 ]]; then
+                    $InstallMethod makecache >/dev/null 2>&1
+                    $InstallMethod install python3 -y >/dev/null 2>&1
+                    alias python="python3"
+                else
+                    $InstallMethod makecache >/dev/null 2>&1
+                    $InstallMethod install python3 -y >/dev/null 2>&1
+                fi
+                
+                elif [ "$is_termux" == 1 ]; then
+                echo -e "${Font_Green}Installing python3${Font_Suffix}"
+                $InstallMethod update -y >/dev/null 2>&1
+                $InstallMethod install python3 -y >/dev/null 2>&1
+                alias python="python3"
+                
+                elif [ "$is_macos" == 1 ]; then
+                echo -e "${Font_Green}Installing python3${Font_Suffix}"
+                $InstallMethod install python3
+                alias python="python3"
+            fi
+        fi
+    fi
+    
+    if ! command -v dig &>/dev/null; then
+        if [ "$is_debian" == 1 ]; then
+            echo -e "${Font_Green}Installing dnsutils${Font_Suffix}"
+            $InstallMethod update >/dev/null 2>&1
+            $InstallMethod install dnsutils -y >/dev/null 2>&1
+            elif [ "$is_redhat" == 1 ]; then
+            echo -e "${Font_Green}Installing bind-utils${Font_Suffix}"
+            $InstallMethod makecache >/dev/null 2>&1
+            $InstallMethod install bind-utils -y >/dev/null 2>&1
+            elif [ "$is_termux" == 1 ]; then
+            echo -e "${Font_Green}Installing dnsutils${Font_Suffix}"
+            $InstallMethod update -y >/dev/null 2>&1
+            $InstallMethod install dnsutils -y >/dev/null 2>&1
+            elif [ "$is_macos" == 1 ]; then
+            echo -e "${Font_Green}Installing bind${Font_Suffix}"
+            $InstallMethod install bind
+        fi
+    fi
+    
+    if ! command -v jq &>/dev/null; then
+        if [ "$is_debian" == 1 ]; then
+            echo -e "${Font_Green}Installing jq${Font_Suffix}"
+            $InstallMethod update >/dev/null 2>&1
+            $InstallMethod install jq -y >/dev/null 2>&1
+            elif [ "$is_redhat" == 1 ]; then
+            echo -e "${Font_Green}Installing jq${Font_Suffix}"
+            $InstallMethod makecache >/dev/null 2>&1
+            $InstallMethod install jq -y >/dev/null 2>&1
+            elif [ "$is_termux" == 1 ]; then
+            echo -e "${Font_Green}Installing jq${Font_Suffix}"
+            $InstallMethod update -y >/dev/null 2>&1
+            $InstallMethod install jq -y >/dev/null 2>&1
+            elif [ "$is_macos" == 1 ]; then
+            echo -e "${Font_Green}Installing jq${Font_Suffix}"
+            $InstallMethod install jq
+        fi
+    fi
+    
+    if [ "$is_macos" == 1 ]; then
+        if ! command -v md5sum &>/dev/null; then
+            echo -e "${Font_Green}Installing md5sha1sum${Font_Suffix}"
+            $InstallMethod install md5sha1sum
+        fi
+    fi
+    
+}
+checkDependencies
+
+local_ipv4=$(curl $curlArgs -4 -s --max-time 10 cloudflare.com/cdn-cgi/trace | grep ip | awk -F= '{print $2}')
+local_ipv4_asterisk=$(awk -F"." '{print $1"."$2".*.*"}' <<<"${local_ipv4}")
+local_ipv6=$(curl $curlArgs -6 -s --max-time 20 cloudflare.com/cdn-cgi/trace | grep ip | awk -F= '{print $2}')
+local_ipv6_asterisk=$(awk -F":" '{print $1":"$2":"$3":*:*"}' <<<"${local_ipv6}")
+local_isp4=$(curl $curlArgs -s -4 --max-time 10 --user-agent "${UA_Browser}" "https://api.ip.sb/geoip/" | jq '.organization' | tr -d '"' &)
+local_isp6=$(curl $curlArgs -s -6 --max-time 10 --user-agent "${UA_Browser}" "https://api.ip.sb/geoip/" | jq '.organization' | tr -d '"' &)
+
+ShowRegion() {
+    echo -e "${Font_Yellow} ---${1}---${Font_Suffix}"
+}
+
+function detect_isp() {
+    local lan_ip=$(echo "$1" | grep -Eo "^(10\.[0-9]{1,3}\.[0-9]{1,3}\.((0\/([89]|1[0-9]|2[0-9]|3[012]))|([0-9]{1,3})))|(172\.(1[6789]|2\[0-9]|3[01])\.[0-9]{1,3}\.[0-9]{1,3}(\/(1[6789]|2[0-9]|3[012]))?)|(192\.168\.[0-9]{1,3}\.[0-9]{1,3}(\/(1[6789]|2[0-9]|3[012]))?)$")
+    if [ -n "$lan_ip" ]; then
+        echo "LAN"
+        return
+    else
+        local res=$(curl $curlArgs --user-agent "${UA_Browser}" -s --max-time 20 "https://api.ip.sb/geoip/$1" | jq ".isp" | tr -d '"' )
+        echo "$res"
+        return
+    fi
+}
+
+function echo_Result() {
+    for((i=0;i<${#array[@]};i++))
+    do
+        echo "$result" | grep "${array[i]}"
+        # sleep 0.03
+    done;
+}
+
+if [ -n "$func" ]; then
+    echo -e "${Font_Green}IPv4:${Font_Suffix}"
+    $func 4
+    echo -e "${Font_Green}IPv6:${Font_Suffix}"
+    $func 6
+    exit
+fi
+
 function Openai_UnlockTest() {
     echo "==============[ Openai ]==============="
     local tmpresult=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -SsLI --max-time 10 "https://chat.openai.com" 2>&1)
@@ -103,7 +357,6 @@ function CheckV6() {
         fi
     fi
 }
-
 
 function ScriptTitle() {
     if [[ "$language" == "e" ]]; then
