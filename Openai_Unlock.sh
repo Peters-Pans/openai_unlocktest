9c76a5a6eeadb7bc4aa122fda1191cfdbbb6f9ef
Peters-Pans <69051746+Peters-Pans@users.noreply.github.com>
2023-09-13 16:33:39 +0800

    Update Openai_Unlock.sh

--- Openai_Unlock.sh
+++ Openai_Unlock.sh
@@ -18,6 +18,24 @@ function Openai_UnlockTest() {
 }
 
 
+function ScriptTitle() {
+    if [[ "$language" == "e" ]]; then
+        echo -e " [Stream Platform & Game Region Restriction Test]"
+        echo ""
+        echo -e "${Font_Green}Github Repository:${Font_Suffix} ${Font_Yellow} https://github.com/1-stream/RegionRestrictionCheck ${Font_Suffix}"
+        echo -e "${Font_Purple}Supporting OS: CentOS 6+, Ubuntu 14.04+, Debian 8+, MacOS, Android (Termux), iOS (iSH)${Font_Suffix}"
+        echo ""
+        echo -e " ** Test Starts At: $(date)"
+        echo ""
+    else
+        # echo -e "${Font_Purple}脚本适配OS: IDK${Font_Suffix}"
+        echo ""
+        echo -e " ** 测试时间: $(date '+%Y-%m-%d %H:%M:%S %Z')"
+        echo ""
+    fi
+}
+ScriptTitle
+
 function RunScript() {
     clear
     ScriptTitle
@@ -29,7 +47,7 @@ function RunScript() {
     if [[ "$isv6" -eq 1 ]]; then
         Openai_UnlockTest 6
     fi
-        goodbye
+
 }
 wait
 RunScript
